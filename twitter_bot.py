942f698d39ab6585e989c11ccf6e78ee50fd66ad
saksham4106 <saksham.vvip@gmail.com>
2020-11-08 13:27:40 +0530

    added GUI

--- twitter_bot.py
+++ twitter_bot.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from tkinter import *
 
 class SendTweets:
     def __init__(self, tweetNo, tweet, timeIn, passIn, userIn):
@@ -36,7 +37,7 @@ class SendTweets:
     
     def tweetMessage(self):
         SpamBarrier = "."
-        for a in range(0, self.tweetNo)
+        for a in range(0, self.tweetNo):
             time.sleep(self.timeIn)
             tweet = self.driver.find_element_by_xpath('''//*[@id='react-root']/div/div/div[2]/main/div/div/div/div/div
                                                         /div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div
@@ -47,14 +48,53 @@ class SendTweets:
             SpamBarrier += "."
 
 
-if __name__ == "__main__":
-    tweetNo = int(input("Enter the Number of tweets (integer) you want to send: "))
-    tweet = input("Enter the tweet(sentence): ")
-    timeIn = int(input("Enter the buffer time (integer) between each tweet"))
-    passIn = input("Enter your account Password: ")
-    userIn = input("Enter your account Username: ")
+
+def startProgram(entry1, entry2, entry3, entry4, entry5):
+        
+    
+    tweetNo = int(entry1.get())
+    tweet = entry2.get()
+    timeIn = int(entry3.get())
+    passIn = entry5.get()
+    userIn = entry4.get()
     Obj = SendTweets(tweetNo, tweet, timeIn, passIn, userIn)
     Obj.login()
+
+if __name__ == "__main__":
+    win = Tk()
+    win.geometry("300x300")
+    label1 = Label(win, text = "Nuber of tweets")
+    label1.place(x=0,y=10)
+    entry1 = Entry(win)
+    entry1.place(x=90,y=10)
+
+    label2 = Label(win, text = "Enter the Tweet")
+    label2.place(x=0, y = 30)
+    entry2 = Entry(win)
+    entry2.place(x=90, y = 30)
+
+    label3 = Label(win, text = "Enter the Time between Each Tweet")
+    label3.place(x = 0, y = 50)
+    entry3 = Entry(win)
+    entry3.place(x = 0, y = 70)
+
+    label4 = Label(win, text = "Twitter Username")
+    label4.place(x=0, y = 100)
+    entry4 = Entry(win)
+    entry4.place(x=100, y = 100)
+
+    label5 = Label(win, text = "Twitter Password")
+    label5.place(x=0, y = 130)
+    entry5 = Entry(win, show = "*")
+    entry5.place(x=100, y = 130)
+
+
+
+    button1 = Button(win, text = "Start", command = lambda:startProgram(entry1,entry2,entry3,entry4,entry5))
+    button1.place(x = 50, y = 200)
+
+
+    win.mainloop()
 
 
 
